8abc4504e74d4be6b7e56dc808b3b3b31b4dde4c
keonp <keonlmp@gmail.com>
2022-08-19 11:44:18 -0400

    api functionality :smiling_imp:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import Search from './components/Search.js';
 
 function App() {
+
+  // /words?ml=duck&sp=b*'
+  
+
+
+
   return (
     <div className="App">
       <h1>Backronym Generator</h1>
+      <Search />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,65 @@
+import axios from 'axios';
+import { useState } from 'react';
+import shuffle from '../utilities/shuffle.js';
+
 function Search() {
-  return <div>Search</div>;
+  const [letter, setLetter] = useState('');
+  const [letterArr, setLetterArr] = useState([]);
+  const [randomArray, setRandomArray] = useState([]);
+  const [chosenWords, setChosenWords] = useState([]);
+  
+
+  // Returns a copy of an array that includes the first 10 elements
+  function subArray(array) {
+    return array.slice(0, 10);
+  }
+
+  function handleInput(e) {
+    setLetter(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    axios({
+      url: `https://api.datamuse.com/words?sp=${letter}*`,
+      method: 'GET',
+      dataResponse: 'json'
+    }).then((response) => {
+      setRandomArray(subArray(shuffle(response.data)));
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+ 
+
+  return (
+    <div>
+      <form>
+        <label htmlFor='search'>Search</label>
+        <input id='search' className='searchInput' type='text' onChange={handleInput} placeholder='Enter a letter'/>
+        <button onClick={(e) => handleSubmit(e)}>hi</button>
+      </form>
+
+      <div>
+        <ul>
+        
+        {
+          randomArray?.map((word) => {
+            return(
+                <li key={word.score}>
+                  <input id='word' type='checkbox' className='wordListItem'/>
+                  <label htmlFor='word'>{word.word}</label>
+                </li>           
+            )
+          })
+      }
+        </ul>
+        <button onClick={(e) => handleSubmit(e)}>Refresh</button>
+      </div>
+    </div>
+    
+  )
 }
 
 export default Search;
