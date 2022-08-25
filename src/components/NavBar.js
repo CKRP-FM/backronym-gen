b33225a3f321dfed34d26cc82e377c7bf9cb0c55
Ping Yu <pingyuuu@hotmail.com>
2022-08-25 00:22:07 -0400

    integrated about page with the nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,13 @@ import SignUp from './SignUp';
 import { useState } from 'react';
 import { useUserAuth } from '../context/UserAuthContext';
 import ErrorModal from './ErrorModal';
+import AboutModal from './AboutModal';
 
 function NavBar() {
   const [showLoginModal, setShowLoginModal] = useState(false);
   const [showSignUpModal, setShowSignUpModal] = useState(false);
+  // this show is for the about modal, to rename
+  const [show, setShow] = useState(false);
   const [error, setError] = useState('');
 
   const { user, logOut } = useUserAuth();
@@ -38,6 +41,14 @@ function NavBar() {
             Currently logged in as <span className="loggedInUser">{user.email ? user.email : 'Anonymous'}</span>
           </p>
         ) : null}
+
+        <div className="aboutPopOut">
+          <button className="aboutBtn" onClick={() => setShow(true)}>
+            ?
+          </button>
+          {/* whenever this method is called it will close the modal */}
+          <AboutModal onClose={() => setShow(false)} show={show} />
+        </div>
 
         <ul className="navLinks">
           {/* for when user is on user page, can go back to home */}
