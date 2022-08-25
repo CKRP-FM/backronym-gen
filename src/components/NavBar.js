656e94efbde43a5f7da26483a95f78971bd554e8
Ping Yu <pingyuuu@hotmail.com>
2022-08-25 00:58:48 -0400

    improved responsiveness of nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -36,44 +36,44 @@ function NavBar() {
     <div className="wrapper">
       {error ? <ErrorModal errorMsg={error} setError={setError} /> : null}
       <nav className="mainNav">
-        {user ? (
-          <p>
-            Currently logged in as <span className="loggedInUser">{user.email ? user.email : 'Anonymous'}</span>
-          </p>
-        ) : null}
-
-        <div className="aboutPopOut">
-          <button className="aboutBtn" onClick={() => setShow(true)}>
-            ?
-          </button>
-          {/* whenever this method is called it will close the modal */}
-          <AboutModal onClose={() => setShow(false)} show={show} />
+        <div className="navLinksContainer">
+          <ul className="navLinks">
+            {/* for when user is on user page, can go back to home */}
+            <li>
+              {' '}
+              <a href="/">Home</a>
+            </li>
+            {user === null ? (
+              <li>
+                <button onClick={(e) => toggleLoginModal(e)}>Log In</button>
+              </li>
+            ) : null}
+            {user === null ? (
+              <li>
+                <button onClick={(e) => toggleSignUpModal(e)}>Sign Up</button>
+              </li>
+            ) : null}
+            {user !== null ? (
+              <li>
+                <button type="submit" onClick={(e) => handleUserLogOut(e)}>
+                  Log Out
+                </button>
+              </li>
+            ) : null}
+          </ul>
+          {user ? (
+            <p>
+              Currently logged in as <span className="loggedInUser">{user.email ? user.email : 'Anonymous'}</span>
+            </p>
+          ) : null}
+          <div className="aboutPopOut">
+            <button className="aboutBtn" onClick={() => setShow(true)}>
+              ?
+            </button>
+            {/* whenever this method is called it will close the modal */}
+            <AboutModal onClose={() => setShow(false)} show={show} />
+          </div>
         </div>
-
-        <ul className="navLinks">
-          {/* for when user is on user page, can go back to home */}
-          <li>
-            {' '}
-            <a href="/">Home</a>
-          </li>
-          {user === null ? (
-            <li>
-              <button onClick={(e) => toggleLoginModal(e)}>Log In</button>
-            </li>
-          ) : null}
-          {user === null ? (
-            <li>
-              <button onClick={(e) => toggleSignUpModal(e)}>Sign Up</button>
-            </li>
-          ) : null}
-          {user !== null ? (
-            <li>
-              <button type="submit" onClick={(e) => handleUserLogOut(e)}>
-                Log Out
-              </button>
-            </li>
-          ) : null}
-        </ul>
       </nav>
       {showLoginModal ? <Login toggleLoginModal={toggleLoginModal} /> : null}
       {showSignUpModal ? <SignUp toggleSignUpModal={toggleSignUpModal} /> : null}
