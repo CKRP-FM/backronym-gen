d5fcc71e94cb59e0a88d8d67fc60d6d495d3a458
Chow <contact-chxw@gmail.com>
2022-08-25 20:11:10 -0400

    restyled error modal to be center on page

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -3,17 +3,19 @@ import React from 'react';
 function ErrorModal({ errorMsg, setError }) {
   return (
     <div className="errorModal">
-      <p className="errorMessage">{errorMsg}</p>
-      {/* Close error pop up and reset error state when user clicks x */}
-      <button
-        onClick={(e) => {
-          e.preventDefault();
-          setError('');
-        }}
-        className="closeButton"
-      >
-        X
-      </button>
+      <div className="errorMsgContainer">
+        <p className="errorMessage">{errorMsg}</p>
+        {/* Close error pop up and reset error state when user clicks x */}
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            setError('');
+          }}
+          className="closeButton"
+        >
+          X
+        </button>
+      </div>
     </div>
   );
 }
