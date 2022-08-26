44855c1a611f7c49935eb86ee6e193678843b698
keonp <keonlmp@gmail.com>
2022-08-26 13:02:17 -0400

    profile page styling

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,6 +1,6 @@
+import { FaRegTrashAlt, FaRegHeart } from 'react-icons/fa';
 import firebase from '../firebase';
-import { getDatabase, ref, onValue, remove } from 'firebase/database';
-
+import { getDatabase, ref, onValue, remove, update } from 'firebase/database';
 import { Link, useParams } from 'react-router-dom';
 import { useUserAuth } from '../context/UserAuthContext';
 import { useEffect, useState } from 'react';
@@ -13,6 +13,17 @@ function UserProfile() {
     const [error, setError] = useState('');
     const [backronymKeys, setBackronymKeys] = useState([]);
     const { uid } = useParams();
+
+    //updating like count
+    function handleLike(resultKey, resultLikes) {
+        const updatedLikes = {
+        likes: resultLikes + 1,
+        };
+
+        const database = getDatabase(firebase);
+        const childRef = ref(database, `/${resultKey}`);
+        update(childRef, updatedLikes);
+    }
 
     // delete entry
     function handleDelete(e, resultKey) {
@@ -67,6 +78,7 @@ function UserProfile() {
                     email: data[key].email,
                     userInput: data[key].userInput,
                     results: data[key].results,
+                    likes: data[key].likes,
                 })
             }
             setBackronymKeys(tempKeyState);
@@ -84,9 +96,24 @@ function UserProfile() {
                         return(
                             (result.email === user.email || (result.email === 'anonymous' && user.email === null)) ?
                             <li className="galleryCard" key={result.key}>
-                                <button className="deleteBtn" onClick={(e) => handleDelete(e, result.key)}>
-                                    X
-                                </button>
+                                <div className="userGalleryControls">
+
+                                    <button className="deleteBtn" onClick={(e) => handleDelete(e, result.key)}>
+                                        <span className="sr-only">Delete</span>
+                                        <FaRegTrashAlt />
+                                    </button>
+
+                                    <button
+                                        className="likeBtn"
+                                        onClick={() => {
+                                            handleLike(result.key, result.likes);
+                                        }}
+                                        >
+                                        <FaRegHeart />
+                                        <p className="likeCount">{result.likes}</p>
+                                    </button>
+
+                                </div>
                                 <h3>{result.userInput}</h3>
                                 {
                                     result.results.map((initialWord, index) => {
@@ -101,10 +128,10 @@ function UserProfile() {
 
             <div className='profileButtons'>
                 <Link to="/">
-                    <button>Back</button>
+                    <button className='backButton'>Back</button>
                 </Link>
-                <Link to="/">
-                    <button onClick={(e) => handleUserAccountDeletion(e)}
+                <Link to="/login">
+                    <button className='deleteProfileButton' onClick={(e) => handleUserAccountDeletion(e)}
                     >Delete Account</button>
                 </Link>
             </div>
