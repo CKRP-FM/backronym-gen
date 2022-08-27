eede1fa62e163520dbeb8253207c471d631e5697
Ping Yu <pingyuuu@hotmail.com>
2022-08-27 03:01:00 -0400

    mvp fix to limit likes per session: user can only like a backronym once

--- src/components/GalleryCard.js
+++ src/components/GalleryCard.js
@@ -1,15 +1,17 @@
 import { FaRegTrashAlt } from 'react-icons/fa';
 import DeleteConfirmation from './DeleteConfirmation.js';
 import { getDatabase, ref, remove, update } from 'firebase/database';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import firebase from '../firebase.js';
 import { useUserAuth } from '../context/UserAuthContext.js';
+import ErrorModal from './ErrorModal.js';
 
 function GalleryCard({ result }) {
   const [deleteWarning, setDeleteWarning] = useState(false);
   const [deleteID, setDeleteID] = useState('');
   // // track whether user has already liked a backronym
-  // const [likedStatus, setLikedStatus] = useState('');
+  const [likedStatus, setLikedStatus] = useState('');
+  const [error, setError] = useState('');
 
   let { user } = useUserAuth();
 
@@ -26,16 +28,30 @@ function GalleryCard({ result }) {
 
   //updating like count
   function handleLike(resultKey, resultLikes) {
-    const updatedLikes = {
-      likes: resultLikes + 1,
-    };
+    // check if the user has already liked this specific backronym
+    const liked = localStorage.getItem(`${result.key}`);
+    setLikedStatus(liked);
 
-    const database = getDatabase(firebase);
-    const childRef = ref(database, `/${resultKey}`);
-    update(childRef, updatedLikes);
+    if (likedStatus !== 'liked') {
+      const updatedLikes = {
+        likes: resultLikes + 1,
+      };
+
+      const database = getDatabase(firebase);
+      const childRef = ref(database, `/${resultKey}`);
+      update(childRef, updatedLikes);
+
+      // // set liked status to local storage
+      localStorage.setItem(`${result.key}`, 'liked');
+    } else {
+      setError("You've already liked this!");
+    }
   }
+
   return (
     <li className="galleryCard" key={result.key}>
+      {error ? <ErrorModal errorMsg={error} setError={setError} /> : null}
+
       <h3>{result.userInput}</h3>
       {/* mapping over each user's submission results array item (each word in array is the initial) */}
       {result.results.map((initialWord, index) => {
