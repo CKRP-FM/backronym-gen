649c2abfc994f2d0b511c1ce85d7c6721d69c928
Ping Yu <pingyuuu@hotmail.com>
2022-08-26 20:12:41 -0400

    set up form on ngram viewer page

--- src/pages/NgramViewer.js
+++ src/pages/NgramViewer.js
@@ -1,9 +1,11 @@
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function NgramViewer() {
+  const [searchInput, setSearchInput] = useState('');
+
   useEffect(() => {
     axios({
       url: 'https://intense-dusk-96795.herokuapp.com/https://books.google.com/ngrams/json?content=Churchill&year_start=1800&year_end=2000&corpus=26&smoothing=3',
@@ -29,6 +31,15 @@ function NgramViewer() {
 
   // getNgram();
 
+  const handleInput = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log('hi');
+  };
+
   return (
     <div className="ngramViewer">
       <NavBar />
@@ -38,8 +49,8 @@ function NgramViewer() {
           <p>Powered by Google Ngram Viewer API</p>
           <h2>What is the Google Ngram Viewer?</h2>
           <p>
-            It is an online search engine that charts the frequencies of any set of search strings using a yearly count
-            of{' '}
+            It is an online search engine that charts the frequencies of any set of search strings, like words or a
+            phrase, using a yearly count of{' '}
             <span className="tooltip">
               n-grams
               <span className="tooltipText">
@@ -55,6 +66,27 @@ function NgramViewer() {
               Source: <a href="https://en.wikipedia.org/wiki/Google_Ngram_Viewer">Wikipedia</a>
             </span>
           </p>
+          <div className="visualizerContainer">
+            <form className="ngramForm">
+              <fieldset>
+                <legend>Enter a word, several words (seperated by a comma) or a phrase:</legend>
+                <label htmlFor="searchNgram" class="sr-only">
+                  Search for a word or phrase
+                </label>
+                <input type="text" id="searchNgram" onChange={handleInput} placeholder="Frankenstein, Dracula..." />
+              </fieldset>
+              <fieldset>
+                <legend>Or pick from our existing backronyms and check their frequency in printed sources! </legend>
+                <label htmlFor="savedBackronyms" class="sr-only">
+                  Search for the frenquency of a saved backronym
+                </label>
+                <select name="savedBackronyms" id="savedBackronyms">
+                  <option value="hi">hi</option>
+                </select>
+              </fieldset>
+              <button onClick={(e) => handleSearchSubmit(e)}>Search</button>
+            </form>
+          </div>
         </section>
       </main>
       <Footer />
